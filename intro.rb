81e7d54ce5ba6db15d84ec1410bd5c064a4a73a5
KUGAKU <githubgemini.0521@gmail.com>
2020-03-21 14:40:06 +0900

    add more

--- intro.rb
+++ intro.rb
@@ -2,33 +2,38 @@ count = 0
 
 live_loop :intro_loop_1 do
   play 99
-  if count < 3 then
+  if count < 4 then
     sleep 1
-  else
-    sleep 0.5
-  end
-  play 94
-  if count < 3 then
+    play 94
     sleep 1
-  else
-    sleep 0.5
-  end
-  play 90
-  if count < 3 then
+    play 90
     sleep 1
-  else
-    sleep 0.5
-  end
-  play 87
-  if count < 3 then
+    play 87
     sleep 1
-  else
+  elsif 3 < count && count < 8
     sleep 0.5
-  end
-  if count < 3 then
-    #sleep 1
+    play 94
+    sleep 0.5
+    play 90
+    sleep 0.5
+    play 87
+    sleep 0.5
+  elsif 7 < count && count < 12
+    sleep 0.25
+    play 94
+    sleep 0.25
+    play 90
+    sleep 0.25
+    play 87
+    sleep 0.25
   else
-    #sleep 0.5
+    sleep 0.125
+    play 94
+    sleep 0.125
+    play 90
+    sleep 0.125
+    play 87
+    sleep 0.125
   end
   count = count + 1
 end
@@ -41,4 +46,3 @@ live_loop :intro_loop_2 do
   sleep 0.5
 end
 =end
-
